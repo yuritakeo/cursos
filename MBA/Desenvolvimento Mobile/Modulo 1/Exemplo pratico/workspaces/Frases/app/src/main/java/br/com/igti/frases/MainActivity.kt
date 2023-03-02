db540f64fdf2b760e4dadd7c965cf2ebc532c83e
Yuri Miyazaki <yuritakeo@ucl.br>
2023-03-02 03:46:00 -0300

    Adicionando botao flutuante de abrir tela para add Frases

--- MBA/Desenvolvimento Mobile/Modulo 1/Exemplo pratico/workspaces/Frases/app/src/main/java/br/com/igti/frases/MainActivity.kt
+++ MBA/Desenvolvimento Mobile/Modulo 1/Exemplo pratico/workspaces/Frases/app/src/main/java/br/com/igti/frases/MainActivity.kt
@@ -2,10 +2,16 @@ package br.com.igti.frases
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.igti.frases.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(R.layout.activity_main)
     }
 }
